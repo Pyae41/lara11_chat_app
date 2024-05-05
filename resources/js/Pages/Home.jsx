1b8f5b9276918d8d458cfa43416195359dc28123
Pyae Phyo Zan <pyae418283@gmail.com>
2024-05-05 09:59:15 +0700

    modify layout and add logic

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,14 +1,72 @@
 import ChatLayout from "@/Layouts/ChatLayout";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { useEffect, useRef, useState } from "react";
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
+import ChatHeader from "@/Components/App/ChatHeader";
+import MessageInput from "@/Components/App/MessageInput";
+import Message from "@/Components/App/Message";
 
-const Home = ({ auth }) => {
-    return <>Message</>;
+const Home = ({ selectedConversation = null, messages = null }) => {
+    const [localMessages, setLocalMessages] = useState([]);
+    const messagesCtrRef = useRef(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            messagesCtrRef.current.scrollTop =
+                messagesCtrRef.current.scrollHeight;
+        }, 10);
+    }, [selectedConversation]);
+
+    useEffect(() => {
+        setLocalMessages(messages ? messages.data.reverse() : []);
+    }, [messages]);
+
+    return (
+        <>
+            {!messages && (
+                <div className="flex flex-col gap-8 justify-center items-center text-center h-full opacity-35">
+                    <div className="text-2xl md:text-4xl p-16 text-gray-800">
+                        Please Select Conversation to see messages
+                    </div>
+                    <ChatBubbleLeftRightIcon className="w-32 h-32 inline-block" />
+                </div>
+            )}
+            {messages && (
+                <>
+                    <ChatHeader selectedConversation={selectedConversation} />
+                    <div
+                        ref={messagesCtrRef}
+                        className="flex-1 overflow-y-auto p-5"
+                    >
+                        {localMessages.length === 0 && (
+                            <div className="flex justify-center items-center h-full">
+                                <div className="text-lg text-slate-200">
+                                    No Message Found
+                                </div>
+                            </div>
+                        )}
+                        {localMessages.length > 0 && (
+                            <div className="flex-1 flex flex-col">
+                                {localMessages.map((message) => (
+                                    <Message
+                                        key={message.id}
+                                        message={message}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </div>
+                    <MessageInput conversation={selectedConversation} />
+                </>
+            )}
+        </>
+    );
 };
 
 Home.layout = (page) => {
     return (
         <AuthenticatedLayout user={page.props.auth.user}>
-            <ChatLayout children={page}>Message</ChatLayout>
+            <ChatLayout children={page} />
         </AuthenticatedLayout>
     );
 };
